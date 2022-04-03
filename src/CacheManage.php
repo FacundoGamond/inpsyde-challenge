910029297b059ea1f91a1de41e819e71e5ba96c0
Facundo Gamond <facundogamond@Facundos-MacBook-Air.local>
2022-04-03 13:30:27 -0300

    code styles cache file

--- src/CacheManage.php
+++ src/CacheManage.php
@@ -1,15 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Facundogamond\InpsydeChallenge;
 
 class CacheManage
 {
-    static function WPREST($data)
+    public static function WPREST(mixed $data): ?mixed
     {
         $result = new \WP_REST_Response($data, 200);
-        
+
         // Set headers.
-        $result->set_headers(array('Cache-Control' => 'max-age=3600'));
+        $result->set_headers(['Cache-Control' => 'max-age=3600']);
 
         return $result;
     }
