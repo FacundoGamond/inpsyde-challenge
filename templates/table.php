3f26b089c5c0f876582692fd23447e57c12b7833
Facundo Gamond <facundogamond@Facundos-MacBook-Air.local>
2022-04-14 19:43:53 -0300

    clean up mobile

--- templates/table.php
+++ templates/table.php
@@ -1,16 +1,25 @@
 <div class="inpsyde-challenge__table">
+
     <?php foreach ($users as $user) : ?>
+
         <article class="inpsyde-challenge__table-article">
-            <a class="inpsyde-challenge__id" href="<?php echo esc_url("https://jsonplaceholder.typicode.com/users/{$user['id']}"); ?>" data-id="<?php echo esc_attr($user['id']); ?>">
+            <a class="inpsyde-challenge__id" 
+                href="<?php echo esc_url(get_home_url() . "/wp-json/inpsyde-challenge/get-user-detail?id={$user['id']}"); ?>" 
+                data-id="<?php echo esc_attr($user['id']); ?>">
                 <?php echo esc_html($user['id']) ?>
             </a>
-            <a class="inpsyde-challenge__name" href="<?php echo esc_url("https://jsonplaceholder.typicode.com/users/{$user['id']}"); ?>" data-id="<?php echo esc_attr($user['id']); ?>">
+            <a class="inpsyde-challenge__name" 
+                href="<?php echo esc_url(get_home_url() . "/wp-json/inpsyde-challenge/get-user-detail?id={$user['id']}"); ?>" 
+                data-id="<?php echo esc_attr($user['id']); ?>">
                 <?php echo esc_html($user['name']); ?>
             </a>
-            <a class="inpsyde-challenge__username" href="<?php echo esc_url("https://jsonplaceholder.typicode.com/users/{$user['id']}"); ?>" data-id="<?php echo esc_attr($user['id']); ?>">
+            <a class="inpsyde-challenge__username" 
+                href="<?php echo esc_url(get_home_url() . "/wp-json/inpsyde-challenge/get-user-detail?id={$user['id']}"); ?>" 
+                data-id="<?php echo esc_attr($user['id']); ?>">
                 <?php echo esc_html($user['username']); ?>
             </a>
         </article>
 
     <?php endforeach; ?>
-    <div>
+
+<div>
